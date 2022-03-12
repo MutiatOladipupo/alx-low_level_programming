25b13d4070124438bf9fc6d7eebecbb57c605f30
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-03-11 21:15:34 -0800

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all single digit numbers of 
  * 10 starting from 0, followed by a new line 
- * *Return: Always 0 (Sucess)
+ * Return: Always 0 (Sucess)
  */
 int main(void)
 {
