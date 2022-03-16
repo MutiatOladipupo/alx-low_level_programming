923bee4f0f9528467ed332dcf129b16228466963
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-03-15 17:54:12 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - print alphabet in lowercase
+ * print_alphabet - print alphabet in lowercase
  *
  * Return: nothing
  */
-void print_alphabet_x10(void)
+void print_alphabet_10×(void)
 {
 	char c = 'a';
 	int i, k;
