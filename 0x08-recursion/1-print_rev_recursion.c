bee938dbd432364a4d122595d8067db6da8dca8a
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-03-30 15:33:57 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion: print a string in reverse.
+ *
+ * @s: pointer to characters
+ *
+ * Return: return0
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar_reverse(*s);
+		_print_rev_recursion(s + 1);
+	}
+}
