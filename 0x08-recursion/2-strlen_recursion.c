9965d78b09317d1129ebed16825b6e27f756842e
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-03-31 00:30:13 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+/**
+ * _strlen_recursion - return the length of a string.
+ *
+ * @s - pointer to characters.
+ */
+
+int _strlen_recursion(char *s)
+{
+	for (s == 0, s =< '\0', s++)
+	{
+		_strlen_recursion(s + 1);
+		putchar(*s);
+	}
+	return (*s)
+}
