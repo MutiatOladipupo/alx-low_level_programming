a7ad278f5db89610083300ac79bd6f3b691242c0
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-03-31 07:59:40 -0700

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,12 +14,8 @@ int factorial(int n)
 		return (-1);
 	}
 
-	else if (n >= 0 && n <= 1 && n++)
+	else 
 	{
-		return (1);
+		return (n * factorial(n - 1));
 	}
-
-	n *= factorial(n - 1);
-
-	return (int);
 }
