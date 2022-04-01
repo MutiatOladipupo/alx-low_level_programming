1a771f98fb035c5fc695dcaec5dd51a25f655d3f
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-03-31 18:14:08 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,23 @@
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number to return the square root of.
+ *
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+
+	if (n == 1)
+	{
+		return (1);
+	}
+
+	return (find_sqrt(n, root));
+}
+
