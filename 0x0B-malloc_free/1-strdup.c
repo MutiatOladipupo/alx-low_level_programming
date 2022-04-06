f43b897ac67f9a2a0e523affa7eca874b72d35c5
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-04-05 17:49:58 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,7 +14,7 @@ char *_strdup(char *str)
 	int k = 0;
 	char *A;
 
-	if (str = NULL)
+	if (str == NULL)
 		 
 	{
 		return (NULL);
@@ -34,7 +34,7 @@ char *_strdup(char *str)
 	{
 		A[n] = str[n];
 
-		A[n]; 
+		A[n] != '\0';
 	}
 	return (A);
 }
