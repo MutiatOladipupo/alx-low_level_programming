1fc75dbc62c0f8fc620e85722028fa1a925a1849
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-04-15 15:11:07 -0700

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -15,10 +15,14 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(n, nums);
 
+	if (n == 0)
+		return (0);
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(n, nums);
 
 	va_end(nums);
 
 	return (sum);
+
 }
