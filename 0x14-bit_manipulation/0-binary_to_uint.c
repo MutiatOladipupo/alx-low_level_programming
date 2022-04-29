b654f4412dfe9b2c5e6a74187593a3a55b201cbf
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-04-28 17:01:42 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,27 +4,26 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointer to a string.
- * Return: if (b != '0' && b != '1'), return 0
-           if (b == '\0'), return NULL
-	   else, return the converted number.
+ * Return: return 0 if any of the chars in b is 0 or 1
+ * else return the converted number.
 */
 
 unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
 	unsigned int j = 0;
-	int mult = 1;
+	int mult;
 
 	if (b[i] == '\0')
 	return (0);
 
-	for (i = 0; b[i] != '\0'; i++)
+	for (i = 0, mult = 1; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' || b[i] != '1')
 		{
 			return (0);
 		}
-		else
+		if (b[i] == 1)
 		{
 			j += b[i] * mult;
 			mult *= 2;
