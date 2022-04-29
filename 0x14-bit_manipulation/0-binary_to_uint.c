78a2815c970c22d8fd78c7e20bbd03c23a77ac65
MutiatOladipupo <mutiatoladipupo22@gmail.com>
2022-04-28 18:00:55 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,23 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i = 0;
 	unsigned int j = 0;
+	int mult;
 
 	if (b[i] == '\0')
 	return (0);
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if (b[i] != '0' || b[i] != '1')
+		for (i--, mult = 1; i >= 0; i--, mult *= 2)
 		{
-			return (0);
-		}
-		if (b[i] == '1')
-		{
-			j += b[i] + 2 * j;
+			if (b[i] != '0' || b[i] != '1')
+			{
+				return (0);
+			}
+			if (b[i] == '1')
+			{
+				j += b[i] * mult;
+			}
 		}
 	}
 	return (j);
